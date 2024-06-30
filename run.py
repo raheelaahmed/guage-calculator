eb41f93360ab93615f97e1f5eb7126a8f1a5a689
Raheela Aftab <raheela.aftab@gmail.com>
2024-06-30 11:03:05 +0000

    add changes

--- run.py
+++ run.py
@@ -1,2 +1,11 @@
-import gspread
+
 print("welcome to crochet guage calculator")
+
+length = float(input("Enter the length (in inches): "))  # Get length as float
+number_of_stitches = int(input("Enter the number of stitches: "))  # Get stitches as integer
+
+# Calculate stitches per inch
+stitches_per_inch = number_of_stitches/length 
+
+# Print the result with proper formatting
+print("Number of stitches per inch:", stitches_per_inch)
