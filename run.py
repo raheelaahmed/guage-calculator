afe3e7ac97b66e18fe00c6973fa910a322e5ecd3
Raheela Aftab <raheela.aftab@gmail.com>
2024-07-01 13:12:47 +0000

    add changes

--- run.py
+++ run.py
@@ -2,6 +2,10 @@
 print("welcome to crochet guage calculator")
 # getting using name by input
 name = input("What is your name? ")
+
+#i want to give three options here 1:calulate the guage forany project 2:calculate the number of stiches and rows for blankets from given sizes \n 3:calculate the rquired number of stiches and rows.  for your desired lenth and wdth. of the project
+
+
 print("--------------------------------------------------------------------------------------------")
 
 print("--------------------------------------------------------------------------------------------")
@@ -9,8 +13,10 @@ print("-------------------------------------------------------------------------
 print("--------------------------------------------------------------------------------------------")
 print(f"Thank you, {name}, for using this app!\nThis app will help you calculate the number of stitches per inch.") 
 print("you need to follow these steps to calculate the number of stiches per inch \n 1:Make a swatch\n 2:Count the number of rows\n 3:Count the number of stiches in the row \n 4:Measure the width in inches\n 5:measure the length in inches")
+def calculate_gauge():
 #getting length by user input
-length = float(input("Enter the length (in inches): ")) 
+
+    length = float(input("Enter the length (in inches): "))
 #getting width by user input
 width=float(input("Enter the width (in inches): ")) 
 #getting the number of stiches by user input
@@ -24,13 +30,14 @@ stitches_per_inch = number_of_stitches/width
 
 #Calculate the number of rows per inch
 
-rows_per_inch = number_of_rows/length
+rows_per_inch = number_of_rows/ length
 
 # Print the result 
 print("Number of stitches per inch:", stitches_per_inch)
 print("Number of stitches per inch:", rows_per_inch)
 
-blanket_length=float(input("enter blanket length"))
+
+blanket_length=float(input("enter blanket  length"))
 blanket_width=float(input("enter blanket width"))
 #calculation rows for project
 Rows=blanket_length*rows_per_inch
